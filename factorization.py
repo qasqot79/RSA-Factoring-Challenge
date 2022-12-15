1eadec3c428161b6fa62935dae7ddc57a7d85463
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2022-12-15 19:18:07 +0100

    Update factorization.py

--- factorization.py
+++ factorization.py
@@ -1,21 +1,27 @@
-#/usr/bin/env python3
+#!/usr/bin/python3
 import sys
 
 
-def primef(n):
-    if n <= 3:
-        return int(n)
-    if n % 2 == 0:
-        return 2
-    elif n % 3 == 0:
-        return 3
-    else:
-        for i in range(5, int((n)**0.5) + 1, 6):
-            if n % i == 0:
-                return int(i)
-            if n % (i + 2) == 0:
-                return primef(n/(i+2))
-    return int(n)
+def factorize(num):
+    """ Generate 2 factors for a given number"""
+    factor1 = 2
+    while (num % factor1):
+        if (factor1 <= num):
+            factor1 += 1
+
+    factor2 = num // factor1
+    return (factor2, factor1)
 
 
-print(primef(int(sys.argv[1])))
+if len(sys.argv) != 2:
+    sys.exit(f"Wrong usage: {sys.argv[0]} <file_path>")
+
+filename = sys.argv[1]
+
+file = open(filename, 'r')
+lines = file.readlines()
+
+for line in lines:
+    num = int(line.rstrip())
+    factor2, factor1 = factorize(num)
+    print(f"{num} = {factor2} * {factor1}")
